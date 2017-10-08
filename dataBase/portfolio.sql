748e08056f4b31fe7edc3827185bb8b2292acc51
Guillaume <dussartguillaume@orange.fr>
2017-10-08 17:20:17 +0200

    add culum in db

--- dataBase/portfolio.sql
+++ dataBase/portfolio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le :  ven. 06 oct. 2017 à 16:29
+-- Généré le :  Dim 08 oct. 2017 à 15:19
 -- Version du serveur :  10.1.28-MariaDB
 -- Version de PHP :  7.1.10
 
@@ -61,7 +61,13 @@ CREATE TABLE `competences` (
 --
 
 INSERT INTO `competences` (`competenceID`, `name`, `sentence`, `images`, `titleCompetenceID`, `date`) VALUES
-(2, 'Html5', '', 'html5.png', 3, '2017-03-07');
+(2, 'Html5', '', 'html5.png', 3, '2017-03-07'),
+(9, 'Machintosh', 'Os utilisé 1 ans', 'apple2.svg', 1, '2017-10-16'),
+(10, 'Windows', 'Os utilisé 5 ans', 'windows.svg', 1, '2017-10-15'),
+(11, 'Linux', 'Os principal', 'linux.svg', 1, '2017-10-18'),
+(12, 'Css3', '', 'css3.png', 3, '2017-03-08'),
+(13, 'Js', '', 'js.png', 3, '2017-04-11'),
+(14, 'Php', '', 'php.png', 3, '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -79,13 +85,6 @@ CREATE TABLE `contacts` (
   `message` text NOT NULL,
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `contacts`
---
-
-INSERT INTO `contacts` (`contactID`, `lastname`, `firstname`, `phone`, `email`, `society`, `message`, `date`) VALUES
-(1, 'Guillaume', 'Dussart', '669923390', 'gdussart1@gmail.com', 'dre', 'dre', '2017-10-06');
 
 -- --------------------------------------------------------
 
@@ -107,10 +106,26 @@ CREATE TABLE `images` (
 --
 
 INSERT INTO `images` (`imageID`, `name`, `type`, `size`, `workID`, `folder`) VALUES
-(1, 'arch1.png', 'image/png', '1137947', 1, 'archiduchesse'),
-(2, 'arch2.png', 'image/png', '539120', 1, 'archiduchesse'),
-(3, 'arch3.png', 'image/png', '112442', 1, 'archiduchesse'),
-(4, 'archiduchesse.png', 'image/png', '2117296', 1, 'archiduchesse');
+(5, 'arch1.png', 'image/png', '1137947', 4, 'archiduchesse'),
+(6, 'arch1_s.png', 'image/png', '38571', 4, 'archiduchesse'),
+(7, 'arch2.png', 'image/png', '539120', 4, 'archiduchesse'),
+(8, 'arch3.png', 'image/png', '112442', 4, 'archiduchesse'),
+(9, 'archiduchesse.png', 'image/png', '2117296', 4, 'archiduchesse'),
+(10, 'archiduchesse_s.png', 'image/png', '90632', 4, 'archiduchesse'),
+(11, 'airbnb.png', 'image/png', '3799246', 5, 'airbnb'),
+(12, 'airbnb1.png', 'image/png', '755953', 5, 'airbnb'),
+(13, 'airbnb2.png', 'image/png', '991789', 5, 'airbnb'),
+(14, 'airbnb_s.png', 'image/png', '124954', 5, 'airbnb'),
+(15, 'lebc.png', 'image/png', '261550', 6, 'leboncoin'),
+(16, 'lebc2.png', 'image/png', '193567', 6, 'leboncoin'),
+(17, 'leboncoin.png', 'image/png', '139917', 6, 'leboncoin'),
+(18, 'leboncoin_s.png', 'image/png', '22981', 6, 'leboncoin'),
+(31, 'coachSporstif.png', 'image/png', '911815', 7, 'sportcoach'),
+(32, 'coachSporstif_s.png', 'image/png', '62541', 7, 'sportcoach'),
+(33, 'pcs.png', 'image/png', '2411472', 7, 'sportcoach'),
+(34, 'pcs2.png', 'image/png', '1721142', 7, 'sportcoach'),
+(35, 'pcs3.png', 'image/png', '1346894', 7, 'sportcoach'),
+(36, 'pcs4.png', 'image/png', '808404', 7, 'sportcoach');
 
 -- --------------------------------------------------------
 
@@ -160,7 +175,10 @@ CREATE TABLE `works` (
 --
 
 INSERT INTO `works` (`workID`, `title`, `subtitle`, `techno`, `content`, `url`, `link`, `date`, `online`) VALUES
-(1, 'Archiduchesse', 'Reproduction du site archiduchesse', 'Projet réaliser avec Bootstrap', '', 'archiduchesse', 'https://gustavofringe.github.io/projectArchiduchesse/', '2017-04-12', 0);
+(4, 'Archiduchesse', 'Reproduction du site archiduchesse', 'Projet réaliser avec Bootstrap', '', 'archiduchesse', 'https://gustavofringe.github.io/projectArchiduchesse/', '2017-10-10', 0),
+(5, 'Airbnb', 'Reproduction du site airbnb', 'Projet réaliser à la main', '', 'airbnb', 'https://gustavofringe.github.io/projectAirbnb/', '2017-04-11', 0),
+(6, 'Leboncoin', 'Reproduction du site leboncoin', 'Projet réaliser en parti avec Bootstrap', '', 'leboncoin', 'https://gustavofringe.github.io/projectLeBonCoin/', '2017-10-08', 0),
+(7, 'Sportscoach', 'Création de site de coaching sportif', 'Projet réaliser avec Bootstrap', '', 'sportscoah', 'https://gustavofringe.github.io/projectSportsCoach/', '2017-03-07', 0);
 
 --
 -- Index pour les tables déchargées
@@ -218,19 +236,19 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT pour la table `competences`
 --
 ALTER TABLE `competences`
-  MODIFY `competenceID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `competenceID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT pour la table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `contactID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `contactID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `images`
 --
 ALTER TABLE `images`
-  MODIFY `imageID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `imageID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT pour la table `titleCompetence`
@@ -242,7 +260,7 @@ ALTER TABLE `titleCompetence`
 -- AUTO_INCREMENT pour la table `works`
 --
 ALTER TABLE `works`
-  MODIFY `workID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `workID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Contraintes pour les tables déchargées
